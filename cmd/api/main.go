0063ec9523aa72f0548d4c4c63dc677515ecc403
Prieyuda Akadita S <akaditasustono@gmail.com>
2022-08-29 22:30:22 +0700

    Add handler config and dependencies

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,12 @@
 package main
 
-import "database/sql"
+import (
+	"authentication/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
 
 const WEB_PORT = "8091"
 
@@ -10,5 +16,23 @@ type Config struct {
 }
 
 func main() {
+	log.Println("Starting authentication service...")
+
+	//Connect to DB
+	//...
+
+	//Setup
+	app := Config{}
+	server := &http.Server{
+		Addr:    fmt.Sprintf("0.0.0.0:%s", WEB_PORT),
+		Handler: app.routes(),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println("Authentication service is started")
 
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module authentication
 go 1.18
 
 require golang.org/x/crypto v0.0.0-20220826181053-bd7e27e6170d
+
+require (
+	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	github.com/go-chi/cors v1.2.1
+)
